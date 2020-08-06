f07b87d3c6a32b1e1f373eb11969d2bb0bd04df6
Linkodt <973672492@qq.com>
2020-08-06 13:32:20 +0800

    .

---
+++ 华农学籽/utils/request.js
@@ -0,0 +1,35 @@
+import {resolve,reject} from "core-js/features/promise"
+/*
+	封装uni.request方法，实现异步效果
+	使用方法:
+		this.request({
+			url:"",
+			methods:"",
+			data:{
+				key:value,
+			}
+		}).then(res=>{
+			// res为请求返回的结果
+		})
+*/
+// ES6
+export default (params)=>{
+	// 加载中
+	uni.showLoading({
+		title:"加载中"
+	})
+	return new Promise((resolve,reject)=>{
+		wx.request({
+			...params,
+			success(res){
+				resolve(res);
+			},
+			fail(err){
+				reject(err);
+			},
+			complete(){
+				uni.hideLoading();
+			}
+		})
+	})
+}
